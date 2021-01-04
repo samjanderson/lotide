646d4702e43af452da6ce5e58ce480ef9b03616a
samjanderson <sam.andersonnn@live.ca>
2021-01-04 23:37:15 +0000

    assertEqual function with updated emojis

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+//If the values match, it should print (console.log) the following: Assertion Passed: [actual] === [expected]
+//Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected]
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual(5, 10);
