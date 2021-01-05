6f59966c5f509335b4f76f52bf5e5360ad36ab35
samjanderson <sam.andersonnn@live.ca>
2021-01-05 01:38:11 +0000

    implement tail challenge with assertEqual function

---
+++ tail.js
@@ -0,0 +1,21 @@
+//return everything except the first item (head) of the array
+//your function should not modify the array that is passed in. it should return a new array**
+//edge cases: An array with only one element should yield an empty array for its tail
+//An empty array should yield an empty array for its tail
+
+const tail = (array) => {
+  if (array.length === 1 || array.length === 0) {
+    return [];
+  } else {
+    return array.slice(1);
+  }
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
