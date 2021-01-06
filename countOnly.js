55aed13beaf32721cac5fbd1940dc29b6f225827
samjanderson <sam.andersonnn@live.ca>
2021-01-06 22:33:15 +0000

    countOnly function that looks through an array and object and creates a new object

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+
+const countOnly = function (allItems, itemsToCount) {
+  let totalCountObj = {};
+  for (let item of allItems) {   //loop through each name in the array
+    if (itemsToCount[item] === true) {
+      if (!totalCountObj[item]) {
+        totalCountObj[item] = 1
+      } else {
+        totalCountObj[item] += 1
+      }
+    }
+  }
+  return totalCountObj;
+}
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
