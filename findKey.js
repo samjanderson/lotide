ebed18334494b38b11b24831a24ee19a57963654
samjanderson <sam.andersonnn@live.ca>
2021-01-08 04:46:56 +0000

    find matching key value using callback function

---
+++ findKey.js
@@ -0,0 +1,38 @@
+//takes in an object and a callback
+//scan object and return first key for which callback returns a truthy value
+//else return undefined
+
+const findKey = function (object, callback) {
+  for (let key in object) {  
+    if (callback(object[key])) {   
+      console.log(key)
+      break;
+    }
+  }
+}
+
+// const findKey = function(object, callback) {
+//   for (let keys of Object.keys(object)) {
+//   if (callback(object[keys])) {
+//     return keys;`
+//   }
+
+// if (callback(item) === true
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
